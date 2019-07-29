bed6b2fcf548a456e1f09b83d9dd9491395b743e
anjimie <anjimie@163.com>
2019-07-29 17:52:05 +0800

    movie

--- moviecity/src/main.js
+++ moviecity/src/main.js
@@ -1,15 +1,25 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App'
-import router from './router'
+import App from './App.vue'
+import router from './routers'
+import store from './stores'
+
+import axios from 'axios'
+Vue.prototype.axios = axios;
+
+Vue.filter('setWH',(url , arg)=>{
+    return url.replace(/w\.h/,arg);
+});
+
+import Scroller from '@/components/Scroller'
+Vue.component('Scroller' , Scroller);
+
+import Loading from '@/components/Loading'
+Vue.component('Loading',Loading);
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
   router,
-  components: { App },
-  template: '<App/>'
-})
+  store,
+  render: h => h(App)
+}).$mount('#app')
